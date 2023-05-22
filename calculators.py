cad61f4e9c51879e476a0f4e8624a0d80eb47563
MDBluequasar <dlghkswls112@naver.com>
2023-05-22 17:26:22 +0900

    python 1일차

---
+++ calculators.py
@@ -0,0 +1,22 @@
+a = 3
+b = 4
+# 덧셈 : +, 뺄셈 : -, 곱하기 : *, 나누기 : /, 몫 : //, 나머지 : %
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+# 자료의 형변환
+# 숫자 -> 문자
+c = 10
+d = 20
+# 결과값이 1020이 나오도록 덧셈을 하여라
+answer = str(c) + str(d)
+print(answer)
+
+# 실수 -> 정수
+e = 23333.3333
+# e를 정수로 바뀌어라
+print(int(e))
